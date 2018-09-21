f75f0e868e0b03b29cca706842d48c6413179558
ankithapreddy <42771852+ankithapreddy@users.noreply.github.com>
2018-09-21 10:28:00 +0530

    Update day4.c

--- day4.c
+++ day4.c
@@ -3,12 +3,12 @@
  
 void main()
 {
-    int s, a, b, c, area;
+    float s, a, b, c, area;
  
     printf("Enter the values of a, b and c \n");
-    scanf("%d %d %d", &a, &b, &c);
+    scanf("%f %f %f", &a, &b, &c);
     /* compute s */
     s = (a + b + c) / 2;
     area = sqrt(s * (s - a) * (s - b) * (s - c));
-    printf("Area of a triangle = %d \n", area);
+    printf("Area of a triangle = %f \n", area);
 }
