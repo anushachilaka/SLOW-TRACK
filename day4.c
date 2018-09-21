287d780e4335dbb3457a8237eb8fffdb7f19b79f
ankithapreddy <42771852+ankithapreddy@users.noreply.github.com>
2018-09-21 12:13:29 +0530

    Update day4.c

--- day4.c
+++ day4.c
@@ -19,19 +19,21 @@ float area_of_triangle(float a, float b)
   return area;
 }
 
-the progarm to find the area of the triangle using the function :
+the progarm to find the area of the triangle using the function when base and height are given :
 
-float area_of_triangle(float a,float b)
+float area_of_triangle(float a ,float b)
 {
   float area;
   area=(a*b)/2
    return area;
- ]
+ 
 #include<stdio.h>
 #include<math.h>
   int main()
  {
-   float a, b, area;
-   printf("Enter the values of a and b\n");
-   scanf("%f%f",&a, &b);
-   area
+   float base, height, area;
+   printf("Enter the values of base and height\n");
+   scanf("%f%f",&base,&height);
+   area=area_of_triangle(base,height);
+   printf("The area of triangle is=%f",area);
+  }
