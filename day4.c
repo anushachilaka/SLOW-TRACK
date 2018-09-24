c61e09238f2866771dd4d11ffa8dbee04f367c8f
ankithapreddy <42771852+ankithapreddy@users.noreply.github.com>
2018-09-24 17:13:49 +0530

    Update day4.c

--- day4.c
+++ day4.c
@@ -46,9 +46,9 @@ int main()
   int i,j,row;
   printf("Enter the number of rows\n");
   scanf("%d",&row);
-  for(i=1,i<=row,++i)
+  for(i=1;i<=row;++i)
   {
-   for(j=1,j<=i,++j)
+   for(j=1;j<=i;++j)
    {
     printf("*");
    }
