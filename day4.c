31a04d0062568ff50ca94f9eb5d9e024b19995e3
ankithapreddy <42771852+ankithapreddy@users.noreply.github.com>
2018-09-24 17:33:21 +0530

    Update day4.c

--- day4.c
+++ day4.c
@@ -38,24 +38,7 @@ float area_of_triangle(float a ,float b)
    printf("The area of triangle is=%f",area);
   }
 
- program to print the structure of triangle
-  
-#include<stdio.h>
-int main()
- {
-  int i,j,row;
-  printf("Enter the number of rows\n");
-  scanf("%d",&row);
-  for(i=1;i<=row;++i)
-  {
-   for(j=1;j<=i;++j)
-   {
-    printf("*");
-   }
-   printf("\n");
-  }
-  return 0;
- }
+
   
   
   
